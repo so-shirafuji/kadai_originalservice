add276a084b1f777ff3c635390eac33d512b0714
meh <meh>
2018-06-21 03:22:54 +0000

    added comments on route

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,29 @@
 */
 
 // Login/Registration Handling
+// inChargeOf: girls
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+// inChargeOf: girls
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
+// inChargeOf: girls
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+// inChargeOf: girls
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
+// inChargeOf: girls
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 // Main
+// inChargeOf: girls
 Route::get('/', 'WelcomeController@index')->name('welcome.index');
 
 Route::group(['middleware' => ['auth']], function () {
+    // inChargeOf: guys
     Route::resource('shops', 'ShopsController', ['only' => ['create','show']]);
+    // inChargeOf: guys
     Route::post('favorite', 'ShopUserController@favorite')->name('favorite');
+    // inChargeOf: guys
     Route::delete('favorite', 'ShopUserController@unfavorite')->name('favorite');
+    // inChargeOf: girls
     Route::resource('user', 'UsersController', ['only' => ['show']]);
 });
