92bdb0d57cebcad3abb365200d6cdb77ecbf56c6
EC2 Default User <ec2-user@ip-172-31-1-165.us-east-2.compute.internal>
2018-06-21 03:05:31 +0000

    route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Login/Registration Handling
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+// Main
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('shops', 'ShopsController', ['only' => ['create','show']]);
+    Route::post('favorite', 'ShopUserController@favorite')->name('favorite');
+    Route::delete('favorite', 'ShopUserController@unfavorite')->name('favorite');
 });
