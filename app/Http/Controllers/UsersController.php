b5813a1973970b1fcfb2208c08d46cdf253d79a8
EC2 Default User <ec2-user@ip-172-31-5-153.us-east-2.compute.internal>
2018-06-22 03:09:26 +0000

    four

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User; 
+use App\Micropost; 
+
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate(10);
+        
+         return view('users.index', [
+            'users' => $users,
+         ]);
+     }
+    
+    //   public function show($id)
+    // {
+        // $user = User::find($id);
+        // $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        // $data = [
+            // 'user' => $user,
+            // 'microposts' => $microposts,
+        // ];
+
+        // $data += $this->counts($user);
+
+        // return view('users.show', $data);
+    // }
+    
+    
+    public function favorites($id)
+    {
+        $user = User::find($id);
+        $favorites = $user->favorites()->paginate(10);
+
+        $data = [
+            'user' => $user,
+            'favorites' => $favorites,
+        ];
+
+        $data += $this->counts($user);
+
+        return view('users.favorites', $data);
+    }
+
+   
+    
+    public function store(Request $request, $id)
+    {
+        \Auth::user()->favorite($id);
+        return redirect()->back();
+    }
+
+    public function destroy($id)
+    {
+        \Auth::user()->unfavorite($id);
+        return redirect()->back();
+    }
+    
+}
---
+++ resources/views/users/show.balade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="user-profile">
+        <div class="icon text-center">
+            <img src="{{ Gravatar::src($user->email, 100) . '&d=mm' }}" alt="" class="img-circle">
+        </div>
+        <div class="name text-center">
+            <h1>{{ $user->name }}</h1>
+        </div>
+        <div class="status text-center">
+            
+        </div>
+    </div>
+    <!--@include('items.items', ['items' => $items])-->
+    <!--{!! $items->render() !!}-->
+@endsection
