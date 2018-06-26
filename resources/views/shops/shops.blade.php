d5b413cdcf0ed77d89636ae1aad110e22b7cf558
EC2 Default User <ec2-user@ip-172-31-1-165.us-east-2.compute.internal>
2018-06-26 04:35:09 +0000

    adjust

--- resources/views/shops/shops.blade.php
+++ resources/views/shops/shops.blade.php
@@ -1,13 +1,17 @@
+<link rel="stylesheet" href="{{ secure_asset('css/create.css') }}">
+
+
+
 @if (count($shops) > 0)
     <div class="row">
         @foreach ($shops as $key => $shop)
             <div class="shop">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
-                        <div class="panel-heading text-center">
+                        <div class="panel-heading text-center image trim">
                             <img src="{{ $shop->image_url->shop_image1 }}" alt="" class="">
                         </div>
-                        <div class="panel-body">
+                        <div class="panel-body" >
                             @if ($shop->id)
                                 {{ Form::open(['route' => 'shops.store', 'method' => 'post']) }}
                                     <input type="hidden" name="name" value="{{ $shop->name }}">
@@ -22,15 +26,16 @@
                                     <input type="hidden" name="line" value="{{ $shop->access->line }}">
                                     <input type="hidden" name="station" value="{{ $shop->access->station }}">
                                     <input type="hidden" name="opentime" value="{{ $shop->opentime }}">
-                                    {{ Form::submit('detail') }}
                                 {{ Form::close() }}
                                 <p class="shop-title">{{ $shop->name }}</p>
                             @else
                                 <p class="shop-title">{{ $shop->name }}</p>
                             @endif
-                            <div class="buttons text-center">
+ 
+                            <div class="buttons">
                                 @if (Auth::check())  
-                                     @include('shops.favorite_button', ['shop' => $shop])  
+                                    {{ Form::submit('detail') }}
+                                    @include('shops.favorite_button', ['shop' => $shop])  
                                 @endif 
                             </div>
                         </div>
