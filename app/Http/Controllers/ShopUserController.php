6cb0c3ec5d8a5206242d2ce3013d5504fc569a7c
meh <meh>
2018-06-27 02:54:31 +0000

    fixed bug

--- app/Http/Controllers/ShopUserController.php
+++ app/Http/Controllers/ShopUserController.php
@@ -21,6 +21,19 @@ class ShopUserController extends Controller
         $ins->SetRequestQuery('id', $shopId);
         //send and get http request
         $res = $ins->GetHttpRequest();
+        
+        //format results
+        if(is_object($res->rest->name)){ $res->rest->name = ''; }
+        if(is_object($res->rest->image_url->shop_image1)){ $res->rest->image_url->shop_image1 = '';}
+        if(is_object($res->rest->address)){ $res->rest->address = ''; }
+        if(is_object($res->rest->name_kana)){ $res->rest->name_kana = '';}
+        if(is_object($res->rest->latitude)){ $res->rest->latitude = '';}
+        if(is_object($res->rest->longitude)){$res->rest->longitude = '';}
+        if(is_object($res->rest->category)){$res->rest->category = '';}
+        if(is_object($res->rest->tel)){$res->rest->tel = '';}
+        if(is_object($res->rest->access->line)){$res->rest->access->line = '';}
+        if(is_object($res->rest->access->station)){$res->rest->access->station = '';}
+        if(is_object($res->rest->opentime)){$res->rest->opentime = '';}
 
         // create shop, or get shop if an shop is found
         $shop = shop::firstOrCreate([
